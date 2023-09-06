fbaf7ddb5edc647d74de9edcc502cfe09f1c1c5a
Giovanni Borgogno <giovaborgogno@gmail.com>
2023-09-05 21:45:41 -0300

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+
+const { INFURA_API_URL, SEPOLIA_PRIVATE_KEY } = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: INFURA_API_URL,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
+
+};
